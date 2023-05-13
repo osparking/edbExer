52c8c75542aef908c6c37e8af3bc92de082a6b62
osparking <jbpark03@gmail.com>
2023-05-13 18:21:36 +0900

    config for H2 embedded DB

--- src/main/java/space/bumtiger/edbExer/config/DBConfig.java
+++ src/main/java/space/bumtiger/edbExer/config/DBConfig.java
@@ -1,5 +1,30 @@
 package space.bumtiger.edbExer.config;
 
-public class DBConfig {
+import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class DBConfig {
+	@Bean
+	public DataSource dataSource() {
+
+		var builder = new EmbeddedDatabaseBuilder();
+		var db = builder.setType(EmbeddedDatabaseType.H2) // HSQL or DERBY
+				.addScript("db/schema.sql").addScript("db/data.sql").build();
+		return db;
+	}
+
+	@Bean
+	public JdbcTemplate createJdbcTeamplate() {
+
+		var template = new JdbcTemplate();
+		template.setDataSource(dataSource());
+
+		return template;
+	}
 }
