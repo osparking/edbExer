1313c585646fb07ae71f7b80ea5ae9065294977d
osparking <jbpark03@gmail.com>
2023-05-13 10:49:05 +0900

    insert car records into car table

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO cars(name, price) VALUES('Audi', 52642);
+INSERT INTO cars(name, price) VALUES('Mercedes', 57127);
+INSERT INTO cars(name, price) VALUES('Hyundai', 9000000);
+INSERT INTO cars(name, price) VALUES('Volvo', 29000);
+INSERT INTO cars(name, price) VALUES('Bentley', 350000);
+INSERT INTO cars(name, price) VALUES('Citroen', 21000);
+INSERT INTO cars(name, price) VALUES('Kia', 41400000);
+INSERT INTO cars(name, price) VALUES('Volkswagen', 21600);
